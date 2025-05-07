9c1fe74206171ba31d62c1423b9f0361fcf55e75
dani <165845527+danieIIIe@users.noreply.github.com>
2025-05-06 22:39:22 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synthe = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const textInput = document.getElementById('text-to-speak');
+  const speakButton = document.querySelector('button');
+  const face = document.querySelector('img');
+  let voices = [];
+
+  function populateVoices() {
+    voices = synthe.getVoices();
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+  populateVoices();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  speakButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoiceIndex = voiceSelect.value;
+    if (voices[selectedVoiceIndex]) {
+      utterance.voice = voices[selectedVoiceIndex];
+    }
+
+    utterance.onstart = () => {
+      face.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      face.src = 'assets/images/smiling.png';
+    };
+
+    synthe.speak(utterance);
+  });
 }
